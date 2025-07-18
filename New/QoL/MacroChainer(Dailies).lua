3134e2c892f16d1389f764b9a4b842d23d596d61
Mbarmem <mohd_friends@yahoo.com>
2025-07-19 01:43:44 +0300

    Update

---
+++ New/QoL/MacroChainer(Dailies).lua
@@ -0,0 +1,68 @@
+--[=====[
+[[SND Metadata]]
+author: Mo
+version: 2.0.0
+description: Macro Chainer - Script for running multiple macros in sequence for daily tasks
+plugin_dependencies:
+- Lifestream
+- vnavmesh
+dependencies:
+- source: ''
+  name: SnD
+  type: git
+
+[[End Metadata]]
+--]=====]
+
+--=========================== VARIABLES ==========================--
+
+-------------------
+--    General    --
+-------------------
+
+local macroDone          = false
+local currentEchoTrigger = nil
+local EchoPrefix         = "[MacroChainer]"
+local MacrosToRun        = {
+    { macroName = "MiniCactpot",            echoTrigger = "MiniCactpot"  },
+    { macroName = "TTSeller",               echoTrigger = "TTSeller"     },
+    { macroName = "AlliedSocietiesQuests", 	echoTrigger = "AlliedQuests" },
+}
+
+--=========================== FUNCTIONS ==========================--
+
+function OnChatMessage()
+    local messageType = TriggerData.type
+    local sender = TriggerData.sender
+    local message = TriggerData.message
+
+    if currentEchoTrigger and message:find(currentEchoTrigger) then
+        LogInfo(string.format("%s Detected echo for: %s | Type: %s | Sender: %s | Message: %s", EchoPrefix, currentEchoTrigger, tostring(messageType), tostring(sender), tostring(message) ))
+        macroDone = true
+    end
+end
+
+--=========================== EXECUTION ==========================--
+
+for _, macro in ipairs(MacrosToRun) do
+    currentEchoTrigger = macro.echoTrigger
+    macroDone = false
+
+    LogInfo(string.format("%s Starting macro: %s", EchoPrefix, macro.macroName))
+    yield("/snd run " .. macro.macroName)
+
+    while not macroDone do
+        yield("/wait 1")
+    end
+
+    LogInfo(string.format("%s Completed macro: %s", EchoPrefix, macro.macroName))
+    yield("/wait 1")
+end
+
+currentEchoTrigger = nil
+macroDone = false
+
+LogInfo(string.format("%s All macros completed. Stopping any remaining..!!", EchoPrefix))
+StopRunningMacros()
+
+--============================== END =============================--
