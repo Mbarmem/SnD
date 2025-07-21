bbdef4065c611810e4acf043db8d3204ae2b4501
Mbarmem <mohd_friends@yahoo.com>
2025-07-21 15:32:34 +0300

    Testing bug fix

--- New/GoldSaucer/TTSeller.lua
+++ New/GoldSaucer/TTSeller.lua
@@ -1,7 +1,7 @@
 --[=====[
 [[SND Metadata]]
 author: Mo
-version: 2.0.0
+version: 2.0.1
 description: Triple Triad Seller - Sells your acumulated Triple Triad cards
 plugin_dependencies:
 - TeleporterPlugin
